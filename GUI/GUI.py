2abe9d8c46fd331221a82e825714d8ec596be839
cabustillo13 <cabustillo13@hotmail.com>
2021-02-28 17:20:52 -0300

    :rocket:[Fix] Update methods in GUI files

--- GUI/GUI.py
+++ GUI/GUI.py
@@ -14,7 +14,7 @@ class Window(QMainWindow):
     def __init__(self):
         super(Window, self).__init__()
         self.setGeometry(100, 100, 500, 300)
-        self.setWindowTitle("PROYECTO REALIDAD VIRTUAL")
+        self.setWindowTitle("PROYECTO MyEP")
         
         # Banner y logo
         self.setWindowIcon(QtGui.QIcon('./Imagenes/ICONO.png'))
@@ -31,23 +31,39 @@ class Window(QMainWindow):
         openFile2.setStatusTip("Enviar Instrucciones al Robot Fisico")
         openFile2.triggered.connect(self.sendInformacion)
         
-        # Deterner envio
+        # Detener envio
         openFile3 = QAction("&Parar Instrucciones", self)
         openFile3.setShortcut("Ctrl+P")
         openFile3.setStatusTip("Detener carga de Instrucciones")
         openFile3.triggered.connect(self.sendRobotF)
         
+        # Ventana Test
+        openFile4 = QAction("&Realizar Test", self)
+        openFile4.setShortcut("Ctrl+T")
+        openFile4.setStatusTip("Corroborar la comunicación Robot y Python")
+        openFile4.triggered.connect(self.ejecutarTest)
+        
+        # Estabelcer comunicacion Real Time
+        openFile5 = QAction("&Establecer comunicación", self)
+        openFile5.setShortcut("Ctrl+I")
+        openFile5.setStatusTip("Ejecutar ordenes en tiempo real")
+        openFile5.triggered.connect(self.sendRealTime)
+        
         self.statusBar()
         mainMenu = self.menuBar()
 
         # Asignar ventanas
-        fileMenu1 = mainMenu.addMenu('&Cargar')
-        fileMenu2 = mainMenu.addMenu('&Enviar')
+        fileMenu1 = mainMenu.addMenu('&Test')
+        fileMenu2 = mainMenu.addMenu('&Real Time')
+        fileMenu3 = mainMenu.addMenu('&Rutina')
         
         # Definir acciones para cada ventana
-        fileMenu1.addAction(openFile1)
-        fileMenu1.addAction(openFile3)
-        fileMenu2.addAction(openFile2)
+        fileMenu1.addAction(openFile4)
+        fileMenu2.addAction(openFile5)
+        fileMenu3.addAction(openFile1)
+        fileMenu3.addAction(openFile2)
+        fileMenu3.addAction(openFile3)
+        
 
         # Background
         self.central_widget = QWidget()               
@@ -66,17 +82,17 @@ class Window(QMainWindow):
     def home(self):
         self.show()
 
-    #Metodo para recibir informacion
+    # Metodo para recibir informacion
     def sendRobotT(self):
         herramientas.controlRobot(True)
         Window.refrescarPantalla(self,'./Imagenes/banner.png')
     
-    #Metodo para enviar informacion
+    # Metodo para enviar informacion
     def sendInformacion(self):
         herramientas.sendArduino()
         Window.refrescarPantalla(self,'./Imagenes/banner.png')
         
-    #Metodo para detener carga de informacion
+    # Metodo para detener carga de informacion
     def sendRobotF(self):
         herramientas.controlRobot(False)
         Window.refrescarPantalla(self,'./Imagenes/banner.png')
@@ -86,6 +102,16 @@ class Window(QMainWindow):
         pixmap = QtGui.QPixmap(path)
         self.lbl.setPixmap(pixmap.scaled(self.lbl.size()))
         
+    # Metodo para ejecutar acciones en tiempo real
+    def sendRealTime(self):
+        herramientas.controlRobot(True)
+        Window.refrescarPantalla(self,'./Imagenes/banner.png')
+        
+    # Metodo para realizar test de conexion
+    def ejecutarTest(self):
+        herramientas.test()
+        Window.refrescarPantalla(self,'./Imagenes/banner.png')
+
 ###############
 ## Ejecucion ##
 ###############
--- GUI/herramientas.py
+++ GUI/herramientas.py
@@ -126,3 +126,128 @@ def sendArduino():
         time.sleep(1.2)
         
     print("Listo") 
+
+###############################################
+## Realizar Test -> Comunicación Half Duplex ##
+###############################################
+
+def test():
+    with serial.Serial('/dev/ttyACM0', 9600, bytesize=8, parity='N', stopbits=1) as ser:
+        # Al inicio recibimos 9 mensajes de parte del Arduino
+        for i in range(9):
+            
+            # Si la linea que estoy leyendo no tiene \n se queda esperando
+            line = ser.readline()   # read a '\n' terminated line
+            # Hacemos la conversion de bit -> string
+            print(line.decode(encoding='UTF-8',errors='strict'))
+            time.sleep(1)
+            
+        while(1):
+            # Enviar comandos
+            data = input("Ingrese orden: ")
+            ser.write(bytes(data,'utf-8'))
+            
+            ser.write(bytes(data + "\r",'utf-8'))
+            print("Enviado")
+            time.sleep(1)
+            
+            #Si la linea que estoy leyendo no tiene \n se queda esperando
+            line = ser.readline()   #
+            print(line.decode(encoding='UTF-8',errors='strict'))
+            time.sleep(1)
+                    
+        #ser.close()
+
+###############
+## Real Time ##
+###############
+
+def realTime(flag):
+    if (flag == True):
+        # Articulacion 0
+        HOST0 = '192.168.1.130'       # The server's hostname or IP address
+        PORT0 = 65430                # The port used by the server
+
+        # Articulacion 1 y 2
+        HOST1 = '192.168.1.131'       # The server's hostname or IP address
+        PORT1 = 65431                # The port used by the server
+
+        # Articulacion 3 y 4
+        HOST3 = '192.168.1.133'       # The server's hostname or IP address
+        PORT3 = 65433                # The port used by the server
+
+        while(True):
+            
+            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s0:
+                s0.connect((HOST0, PORT0))
+                
+                #Dato recibido
+                data0 = float(s0.recv(1024))
+            
+                #La simulacion acepta radianes
+                dataR0 = data0*2.22
+            
+            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s1:
+                s1.connect((HOST1, PORT1))
+                
+                #Dato recibido
+                data = (s1.recv(1024))
+                dataV = data.decode('ASCII')
+                
+                #Separar el string en partes
+                dataN = dataV.split("/")
+                
+                #Altura Gripper
+                data1 =float(dataN[0])
+                #Apertura y cierre Gripper
+                data2 =float(dataN[1])
+                
+                #print(float(data3), float(data4))
+                
+                #La simulacion acepta radianes
+                dataR1 = data1*2.22
+                #Escalamos los valores de la altura para evitar romper la simulacion
+                dataR2 = data2*2.22
+                
+                #Movimiento relativo
+                dataRR1 = dataR1 - dataR0
+                dataRR2 = dataR2 - dataR1 - dataR0
+            
+            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s3:
+                s3.connect((HOST3, PORT3))
+                
+                #Dato recibido
+                data = (s3.recv(1024))
+                dataV = data.decode('ASCII')
+                
+                #Separar el string en partes
+                dataN = dataV.split("/")
+                
+                #Altura Gripper
+                data3 =float(dataN[0])
+                #Apertura y cierre Gripper
+                data4 =float(dataN[1])
+                
+                #print(float(data3), float(data4))
+                
+                #La simulacion acepta radianes
+                dataR3 = data3
+                #Escalamos los valores de la altura para evitar romper la simulacion
+                dataR4 = data4*2.22
+            
+            with serial.Serial('/dev/ttyACM0', 9600, bytesize=8, parity='N', stopbits=1) as ser:
+    
+                # SEND REQUEST
+                ser.write(bytes(dataR0,'utf-8'))
+                time.sleep(1)
+                ser.write(bytes(dataR1,'utf-8'))
+                time.sleep(1)
+                ser.write(bytes(dataR2,'utf-8'))
+                time.sleep(1)
+                ser.write(bytes(dataR3,'utf-8'))
+                time.sleep(1)
+                ser.write(bytes(dataR4,'utf-8'))
+                time.sleep(1)
+                #ser.close()
+    else:
+        print("Proceso terminado")
